e9bd08b94bc1c1d075c051d10d8e0be181ebccf9
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-21 16:58:52 -0400

    Add license headers

--- Sources/AndroidLooper/Extensions/OptionSet.swift
+++ Sources/AndroidLooper/Extensions/OptionSet.swift
@@ -1,9 +1,16 @@
+//===----------------------------------------------------------------------===//
 //
-//  OptionSet.swift
-//  SwiftAndroid
+// This source file is part of the SwiftAndroidNative open source project
 //
-//  Created by Alsey Coleman Miller on 7/6/25.
+// Copyright (c) 2024-2026 Skip.dev and SwiftAndroidNative project authors
+// Licensed under Apache License v2.0
 //
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAndroidNative project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 extension OptionSet {
   // Helper method for building up a comma-separated list of options
