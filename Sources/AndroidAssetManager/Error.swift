654b7c79f37731c3a76fe50d95a52040af9c98af
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-21 16:03:57 -0400

    Add license headers

--- Sources/AndroidAssetManager/Error.swift
+++ Sources/AndroidAssetManager/Error.swift
@@ -1,9 +1,16 @@
+//===----------------------------------------------------------------------===//
 //
-//  Error.swift
-//  SwiftAndroid
+// This source file is part of the SwiftAndroidNative open source project
 //
-//  Created by Alsey Coleman Miller on 2/27/26.
+// Copyright (c) 2024-2026 Skip.dev and SwiftAndroidNative project authors
+// Licensed under Apache License v2.0
 //
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftAndroidNative project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 /// Android file manager error.
 public enum AndroidFileManagerError: Swift.Error, Equatable, Sendable {
