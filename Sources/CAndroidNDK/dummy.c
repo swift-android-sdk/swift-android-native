48bdd0612a8cec8e27793fb8871ab452dbd3aafb
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-22 09:02:43 -0400

    Add weak stubs for NDK functions

--- Sources/CAndroidNDK/dummy.c
+++ Sources/CAndroidNDK/dummy.c
@@ -16,6 +16,22 @@
 
 #include <jni.h>
 #include <android/log.h>
+#include <android/configuration.h>
 #include <dlfcn.h>
+
+// Weak stubs for NDK functions introduced after the minimum supported API level.
+// These are overridden by the real implementations when running on a device that
+// supports them; they exist solely to satisfy the linker for older API targets.
+
+// AConfiguration_getScreenRound / AConfiguration_setScreenRound: introduced in API 30
+__attribute__((weak)) int32_t AConfiguration_getScreenRound(AConfiguration* config) {
+    (void)config;
+    return 0; // ACONFIGURATION_SCREENROUND_ANY
+}
+
+__attribute__((weak)) void AConfiguration_setScreenRound(AConfiguration* config, int32_t value) {
+    (void)config;
+    (void)value;
+}
 
 #endif
